d48a7f0a4ca323261df3921bd6f052ce3c87ea33
Matej Nemček <ybdaba@gmail.com>
2021-04-22 09:34:07 +0200

    init

---
+++ pages/hero.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <section class="hero is-black is-halfheight">
+    <div class="hero-body">
+      <div class="">
+        <p class="title">
+          <img src="@/assets/metaprime-whitefg.png" alt="metaprime - experimental nft landscape">
+        </p>
+        <p class="subtitle">
+          Space for illiquid goods and low velocity appraisal assets
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'hero',
+}
+</script>
