6089c2b3292fc7824ed5cc0c2ddba84df018d227
Matej Nemček <ybdaba@gmail.com>
2021-04-22 12:24:19 +0200

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'metaprime-landing',
+    title: 'metaprime.network',
     htmlAttrs: {
       lang: 'en'
     },
