72251008bf355a03097ea04d0b2a831a31f16dc6
Matej Nemček <ybdaba@gmail.com>
2021-04-22 12:13:01 +0200

    subscribe works

--- pages/getrevue.vue
+++ pages/getrevue.vue
@@ -8,28 +8,16 @@
       <div id="revue-embed">
         <form action="https://www.getrevue.co/profile/metaprime/add_subscriber" method="post" id="revue-form" name="revue-form"  target="_blank">
         <div class="form">
-          <!-- <label for="member_email">Email address</label> -->
-        </div>
-        <b-field label="e-mail address" label-position="on-border">
+          <b-field label="e-mail address" label-position="on-border">
           <b-input class="revue-form-field" placeholder="Your email address..." type="email" name="member[email]" id="member_email" expanded></b-input>
           <p class="control">
-            <b-button class="button is-primary" type="submit" value="Subscribe" name="member[subscribe]" id="member_submit">
+            <b-input class="" type="submit" value="Subscribe" name="member[subscribe]" id="member_submit">
               Subscribe
-            </b-button>
+            </b-input>
           </p>
-        </b-field>
-        <!-- <div class="revue-form-group">
-          <label for="member_first_name">First name <span class="optional">(Optional)</span></label>
-          <input class="revue-form-field" placeholder="First name... (Optional)" type="text" name="member[first_name]" id="member_first_name">
-        </div>
-        <div class="revue-form-group">
-          <label for="member_last_name">Last name <span class="optional">(Optional)</span></label>
-          <input class="revue-form-field" placeholder="Last name... (Optional)" type="text" name="member[last_name]" id="member_last_name">
-        </div> -->
-        <div class="revue-form-actions">
-
-        </div>
-        <div class="revue-form-footer">By subscribing, you agree with Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</div>
+          </b-field>
+          <div class="revue-form-footer">By subscribing, you agree with Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</div>
+          </div>
         </form>
       </div>
     </div>
